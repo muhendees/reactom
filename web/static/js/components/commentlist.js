35a4e49d047720f5d99d36fffdad28d7eceba43f
Muhendees <muhendees@gmail.com>
2015-11-14 20:18:43 +0400

    Added separate React components

---
+++ web/static/js/components/commentlist.js
@@ -0,0 +1,20 @@
+let CommentList = React.createClass({
+  render: function() {
+      console.log(this.props.data);
+
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+export default CommentList
